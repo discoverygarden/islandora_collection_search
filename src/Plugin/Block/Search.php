2d297d9ffb75b7a2065fb284538c702139e09814
willtp87 <willtp87@hotmail.com>
2019-03-19 11:08:15 -0300

    porting block wiring

---
+++ src/Plugin/Block/Search.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\islandora_collection_search\Plugin\Block;
+
+use Drupal\islandora\Plugin\Block\AbstractFormBlockBase;
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ *
+ * @Block(
+ *   id = "islandora_collection_search",
+ *   admin_label = @Translation("Islandora Collection Search"),
+ * )
+ */
+class Search extends AbstractFormBlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return \Drupal::formBuilder()->getForm('islandora_collection_search_form');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function blockAccess(AccountInterface $account) {
+    return AccessResult::allowedIfHasPermission($account, 'search islandora solr');
+  }
+
+}
